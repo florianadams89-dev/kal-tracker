1fc9b6d09442f6b62772c48ed980a6b62a4434b3
florianadams89-dev <florianadams89@gmail.com>
2026-09-05 11:05:34 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = 'v20';
+const VERSION = 'v21b';
 const CACHE = 'kal-tracker-' + VERSION;
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png', './icon-maskable-512.png', './apple-touch-icon.png'];
 
